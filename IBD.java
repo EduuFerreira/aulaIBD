a2237e9d073f270e72173455a0f860ef34ad91cb
EduardoSF <eduardosilvaferreira4@gmail.com>
2025-01-26 21:06:56 -0300

    Terceiro commit com o merge da branch teste.

--- IBD.java
+++ IBD.java
@@ -2,5 +2,6 @@ public class IBD {
     public static void main (String[]args){
         System.out.println("1º [Primeiro commit realizado]");
         System.out.println("2º [Criação da Branch e commit dela]");
+        System.out.println("3º [Finge que arrumei um erro depois do merge]");
     }
 }
